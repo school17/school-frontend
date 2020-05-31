50d18d5e279767eb8c7e7db311a5180fe8b7bc6f
goutham-palani <goutham.palani@freshworks.com>
2020-05-31 11:23:32 +0530

    Attendance table css fix

--- src/utils/attendanceTableStyles.ts
+++ src/utils/attendanceTableStyles.ts
@@ -19,16 +19,29 @@ export const tableRowTheme = createMuiTheme({
         fontSize: '12px',
         fontWeight: 500,
         textAlign: 'center',
-        maxWidth: "200px"
+        maxWidth: "200px",
+        padding: "15px 0px",
+        backgroundColor: "#FFFFFF",
+        "&:nth-child(2n)":{
+          backgroundColor: "#F5F6F8"
+        },
+        "&:nth-child(1)": {
+          padding: "15px 15px"
+        }
       },
       head: {
         fontWeight: 900,
-        padding: 8,
         fontSize: 10,
         textAlign: 'center',
         position: 'sticky',
+        backgroundColor: "#FFFFFF",
+        padding: "12px 5px"
       },
       stickyHeader :{
+        backgroundColor: "#FFFFFF",
+        "&:nth-child(2n)":{
+          backgroundColor: "#F5F6F8"
+        }
       }
     }
   }

d1234ffa41d58824736b6fcf371987ff76716f30
Karthikeyan S <karthikkevin83@gmail.com>
2020-07-18 12:44:02 +0530

    Fixed logwork displaying unwanted data bug. (#24)
    * Fixed logwork displaying uneanted data bug.
    
    * removed Console.Log

--- src/components/grade-dashboard/logWorkComponent.tsx
+++ src/components/grade-dashboard/logWorkComponent.tsx
@@ -1,23 +1,19 @@
-import React, { ReactElement, useState, useEffect } from 'react';
-import {useDashboardPrimaryStyles} from '../../utils/dashboradstyles';
+import React, { ReactElement, useState, useEffect } from "react";
+import { useDashboardPrimaryStyles } from "../../utils/dashboradstyles";
 import { Calendar } from "react-modern-calendar-datepicker";
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { formUseStyles } from "../../utils/formStyles";
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
-import {getMonth, getDay, getToday} from "../../utils/dateUtils";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import { getMonth, getDay, getToday } from "../../utils/dateUtils";
 import { useDispatch, useSelector } from "react-redux";
-import {getTimeTable} from '../../actions/time-table-actions';
-import LogworklistComponent from './logworklistComponent';
-import {
-  makeStyles,
-  Theme,
-  createStyles
-} from "@material-ui/core/styles";
+import { getTimeTable } from "../../actions/time-table-actions";
+import LogworklistComponent from "./logworklistComponent";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
 interface Props {
-  institution:any,
-  grade:any,
-  section:any
+  institution: any;
+  grade: any;
+  section: any;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -28,102 +24,136 @@ const useStyles = makeStyles((theme: Theme) =>
       background: "#FFF",
       borderRadius: 10,
       flexDirection: "column",
-      alignItems: "self-end"
+      alignItems: "self-end",
     },
     date: {
       display: "flex",
       alignItems: "center",
       marginTop: 10,
-      color: "#6DA0E6"
-    }
+      color: "#6DA0E6",
+    },
   })
 );
 
-function LogWorkComponent({institution, grade, section}: Props): ReactElement {
-
+function LogWorkComponent({
+  institution,
+  grade,
+  section,
+}: Props): ReactElement {
   const dashboardClasses = useDashboardPrimaryStyles();
   const dispatch = useDispatch();
   const classes = useStyles();
-  const [selectedDay, setSelectedDay]:any = useState({day: new Date().getDate(), month: new Date().getMonth() + 1, year: new Date().getFullYear()});
-  
-  const [day, setDay] = useState(getToday() === 'Sunday' ? 'Monday' : getToday());
+  const [selectedDay, setSelectedDay]: any = useState({
+    day: new Date().getDate(),
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear(),
+  });
+
+  const [day, setDay] = useState(
+    getToday() === "Sunday" ? "Monday" : getToday()
+  );
   const [toggleCalendar, setToggleCalendar] = useState(false);
 
   const [dayTimeTable, setDayTimeTable] = useState([]);
 
   const setDefaultTimeTable = () => {
-    const daysTimeTable = timetable.timetable.filter((dayTimeTable:any) => {
-      if(dayTimeTable[day]) {
+    const daysTimeTable = timetable.timetable.filter((dayTimeTable: any) => {
+      if (dayTimeTable[day]) {
         return dayTimeTable[day];
       }
     });
     setDayTimeTable(daysTimeTable[0][day]);
-  }
+  };
 
-  const {timetable} = useSelector((store:any) => {
+  const { timetable } = useSelector((store: any) => {
     return store.gradeDashboardReducer;
   });
 
   useEffect(() => {
-    if(Object.keys(timetable).length < 1 || (timetable.grade !== grade || timetable.section !== section)) {
+    if (
+      Object.keys(timetable).length < 1 ||
+      timetable.grade !== grade ||
+      timetable.section !== section
+    ) {
       dispatch(getTimeTable(institution, grade, section));
     }
-    if(Object.keys(timetable).length > 1 ) {
+    if (
+      Object.keys(timetable).length > 1 &&
+      timetable.grade === grade &&
+      timetable.section === section
+    ) {
       setDefaultTimeTable();
     }
-  },[timetable, dayTimeTable, day, grade, section]);
-
+  }, [timetable, dayTimeTable, day, grade, section]);
 
   const displayCalendar = () => {
-    if(toggleCalendar) {
+    if (toggleCalendar) {
       return (
         <Calendar
-            value={selectedDay}
-            onChange={(e:any)=>{setToggleCalendar(false); setSelectedDay(e); updateDay()}}
-            shouldHighlightWeekends
-          />
-      )
-    } 
-  }
+          value={selectedDay}
+          onChange={(e: any) => {
+            setToggleCalendar(false);
+            setSelectedDay(e);
+            updateDay();
+          }}
+          shouldHighlightWeekends
+        />
+      );
+    }
+  };
 
   const updateDay = () => {
-    const date = `${selectedDay.day} ${getMonth(selectedDay.month)}  ${selectedDay.year}`;
+    const date = `${selectedDay.day} ${getMonth(selectedDay.month)}  ${
+      selectedDay.year
+    }`;
     setDay(getDay(new Date(date).getDay()));
-  }
+  };
 
   const renderDate = () => {
-    if(selectedDay) {
-      const date = `${selectedDay.day} ${getMonth(selectedDay.month)}  ${selectedDay.year}`;
+    if (selectedDay) {
+      const date = `${selectedDay.day} ${getMonth(selectedDay.month)}  ${
+        selectedDay.year
+      }`;
       return (
-       <span>{selectedDay.day}, {getMonth(selectedDay.month)}, {getDay(new Date(date).getDay())}</span> 
-      )
+        <span>
+          {selectedDay.day}, {getMonth(selectedDay.month)},{" "}
+          {getDay(new Date(date).getDay())}
+        </span>
+      );
     }
-  }
+  };
 
   const renderLogListComponent = () => {
-    if(dayTimeTable.length > 0) {
+    if (dayTimeTable.length > 0) {
       return (
-        <LogworklistComponent dayTimeTable={dayTimeTable} institution={institution} 
-          grade={grade} section={section} selectedDay={selectedDay}></LogworklistComponent>
-      )
+        <LogworklistComponent
+          dayTimeTable={dayTimeTable}
+          institution={institution}
+          grade={grade}
+          section={section}
+          selectedDay={selectedDay}
+        ></LogworklistComponent>
+      );
     }
-  }
-   
+  };
+
   return (
-    <div className= {dashboardClasses.root}>
+    <div className={dashboardClasses.root}>
       <section className={classes.header}>
         <div>Log Book</div>
         <div className={classes.date}>
-        <CalendarTodayIcon onClick={()=>{setToggleCalendar(true)}}></CalendarTodayIcon>
-        {renderDate()}
+          <CalendarTodayIcon
+            onClick={() => {
+              setToggleCalendar(true);
+            }}
+          ></CalendarTodayIcon>
+          {renderDate()}
         </div>
         {displayCalendar()}
       </section>
-      <div>
-          {renderLogListComponent()}
-      </div>
+      <div>{renderLogListComponent()}</div>
     </div>
-  )
+  );
 }
 
-export default LogWorkComponent
+export default LogWorkComponent;

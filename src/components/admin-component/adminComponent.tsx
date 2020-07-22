3b4f379d7b2f2c867d2f24fe5f037bb9dfedafb8
GO-MADsolutions <gomadsolutions@gmail.com>
2020-07-22 14:30:11 +0530

    Admin Dashboard skeleton (#28)
    Co-authored-by: goutham-palani <goutham.palani@freshworks.com>

--- src/components/admin-component/adminComponent.tsx
+++ src/components/admin-component/adminComponent.tsx
@@ -1,27 +1,11 @@
 import React, { ReactElement } from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import AdminListComponent from "../admin-component/adminListComponent";
+import AdminListComponent from "./examComponent/adminListComponent";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       boxShadow: "1px 0px 15px -1px rgba(0,0,0,0.08)",
-    },
-    ExamImgSize: {
-      position: "absolute",
-      height: "50px",
-      width: "50px",
-      left: "0px",
-    },
-    TeacherImgSize: {
-      position: "absolute",
-      height: "50px",
-      width: "50px",
-      left: "80px",
-    },
-    ImgDiv: {
-      position: "relative",
-      boxShadow: "1px 0px 15px -1px rgba(100,100,100,0.08)",
     },
   })
 );
